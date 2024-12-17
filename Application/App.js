3ef71283c828ac5288ed8f471f096960fc91a87b
krmethax <methasit6061@gmail.com>
2024-12-17 22:42:51 +0700

    demo

--- Application/App.js
+++ Application/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 
 import MainNavigation from './navigation/MainNavigation';
 
 export default function App() {
+  const [fontsLoaded, error] = useFonts({
+    'IBMPlexSansThai-Light': require('./assets/fonts/IBMPlexSansThai-Light.ttf'),
+    'IBMPlexSansThai-Medium': require('./assets/fonts/IBMPlexSansThai-Medium.ttf'),
+    'IBMPlexSansThai-Bold': require('./assets/fonts/IBMPlexSansThai-Bold.ttf'),
+  });
+
+  if (error) {
+    console.error('Error loading font:', error);
+    return <Text></Text>;
+  }
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  Text.defaultProps = Text.defaultProps || {};
+  Text.defaultProps.style = { fontFamily: 'IBMPlexSansThai-Medium' };
+  Text.defaultProps.style = { fontFamily: 'IBMPlexSansThai-Light' };
+  Text.defaultProps.style = { fontFamily: 'IBMPlexSansThai-Bold' };
+
   return (
     <NavigationContainer>
       <MainNavigation />
